f84b6bd61c86fb288c464466b26922da9fda6158
GreyMatterImpulsion <smaroleg@gmail.com>
2020-10-09 22:20:40 +0300

    feat: Task 09 done

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task09.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task09.java
@@ -13,17 +13,37 @@ import java.util.Scanner;
  */
 public class Task09 {
     public static void main(String[] args) {
-        //FIXME
-        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
-
-        // Код необходимый для тестирования, не изменять
         Scanner scanner = new Scanner(System.in);
-        int numberFloors = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[0]);
-        int numberApartmentsPerFloor = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[1]);
-        int apartmentNumber = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[2]);
-
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int numberFloors = args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[0]);
+        int numberApartmentsPerFloor = args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[1]);
+        int apartmentNumber = args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[2]);
+        int apsInOnePorch = numberFloors * numberApartmentsPerFloor;
+        int porch = apartmentNumber / apsInOnePorch;
+        if (apartmentNumber / apsInOnePorch <= 1) {
+            porch = 1;
+        } else if (apartmentNumber / apsInOnePorch > 1) {
+            porch = apartmentNumber / apsInOnePorch + 1;
+        }
+        int apsLeft;
+        if (porch == 1) {
+            apsLeft = apartmentNumber;
+        } else {
+            apsLeft = apartmentNumber - porch * apsInOnePorch;
+        }
+        int floor = 0;
+        if (apsLeft <= numberApartmentsPerFloor && apsLeft > 0) {
+            floor = 1;
+        } else if (apsLeft % numberApartmentsPerFloor == 0) {
+            floor = apsLeft / numberApartmentsPerFloor;
+        } else if (apsLeft < 0) {
+            if ((apsInOnePorch - apsLeft) % numberApartmentsPerFloor == 0) {
+                floor = (apsInOnePorch - apsLeft) / numberApartmentsPerFloor;
+            } else if ((apsInOnePorch - apsLeft) % numberApartmentsPerFloor != 0) {
+                floor = (apsInOnePorch + apsLeft) / numberApartmentsPerFloor + 1;
+            }
+        } else {
+            floor = apsLeft / numberApartmentsPerFloor + 1;
+        }
+        System.out.println("Porch: " + porch + ". Floor: " + floor);
     }
 }
