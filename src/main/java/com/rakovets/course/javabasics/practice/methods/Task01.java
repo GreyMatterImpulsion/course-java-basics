97a67b7b92b57ddcffe64f522e6f8ddc6825e290
GreyMatterImpulsion <smaroleg@gmail.com>
2020-10-10 19:39:59 +0300

    feat: Task 01 Methods done

--- src/main/java/com/rakovets/course/javabasics/practice/methods/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/methods/Task01.java
@@ -27,9 +27,6 @@ public class Task01 extends StandardInputTask {
      * @return true когда вошел, false если не вошел
      */
     static boolean isTopPlayer(int sizeTop, int positionPlayerByDamage) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-        return false;
+        return positionPlayerByDamage <= sizeTop;
     }
 }
