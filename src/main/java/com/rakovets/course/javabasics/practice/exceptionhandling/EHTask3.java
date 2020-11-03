0a79109cf158f3a225e5780e0c6c23524c4ecb46
GreyMatterImpulsion <smaroleg@gmail.com>
2020-11-03 19:18:06 +0300

    Exception Handling Task 3 is Done

---
+++ src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/EHTask3.java
@@ -0,0 +1,22 @@
+package com.rakovets.course.javabasics.practice.exceptionhandling;
+
+import java.util.Random;
+
+public class EHTask3 {
+    final static Random random = new Random();
+
+    public static void main(String[] args) throws MyException {
+        try {
+            for (int j = 0; j <= 10; j++) {
+                if (random.nextInt() % 2 == 0) {
+                    System.out.println("tick-tok");
+                } else {
+                    throw new MyException();
+                }
+            }
+        } catch (MyException e) {
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/MyException.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.javabasics.practice.exceptionhandling;
+
+public class MyException extends Exception {
+
+   public String getMessage() {
+      return "Watch \"Luch\" is broken";
+   }
+}
