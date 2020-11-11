97a7e2cccdc9a010ba251913b6a836021dedf82e
GreyMatterImpulsion <smaroleg@gmail.com>
2020-11-12 01:58:26 +0300

    JavaIO Task 1-4, are Done, Task 5 v govne.

--- src/main/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtil.java
@@ -1,14 +1,15 @@
 package com.rakovets.course.javabasics.practice.javaio;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class FileAnalyzeUtil {
     public static void main(String[] args) {
-       // getFileStrings();
-       // getWordsStartWithVowels();
+        getFileStrings();
+        getWordsStartWithVowels();
         getWordsWithEqualFirstAndLastChars();
+        getIncreasingNumbers();
     }
 
     public static void getFileStrings() {
@@ -104,4 +105,55 @@ public class FileAnalyzeUtil {
             e.printStackTrace();
         }
     }
+
+    public static void getIncreasingNumbers() {
+
+        try (FileInputStream fileInputStream = new FileInputStream(
+                "//D:/home/smargun/dev/course-java-basics/src/main/resources/test2");
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(inputStreamReader)) {
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                stringBuilder.append(stringBuilder.length() == 0 ? "\"" : ", \"");
+                for (int value : splitLineParseAndSort(line)) {
+                    stringBuilder.append(value).append(" ");
+                }
+                int length = stringBuilder.length();
+                stringBuilder.deleteCharAt(length - 1).append("\"");
+            }
+            System.out.print(stringBuilder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+   public static int[] splitLineParseAndSort(String line) {
+       StringBuilder stringBuilder = new StringBuilder();
+       ArrayList<Integer> preResult = new ArrayList<Integer>();
+       ArrayList<Integer> comb = new ArrayList<Integer>();
+       ArrayList<Integer> temp = new ArrayList<Integer>();
+       for (String s: line.split(" ")) {
+           int value = Integer.parseInt(s);
+           preResult.add(value);
+       }
+       temp.add(preResult.get(0));
+       for (int k = 0; k < preResult.size(); k++) {
+           if (((preResult.get(k) - temp.get(temp.size() - 1) == 1))) {
+               temp.add(preResult.get(k));
+           } else {
+               if (temp.size() > comb.size()) {
+                   comb = temp;
+               }
+               temp.clear();
+               temp.add(preResult.get(k));
+           }
+       }
+       int [] result = new int[comb.size()];
+       for (int s: comb) {
+           int i = 0;
+           result[i] = s;
+       }
+       return result;
+   }
 }
---
+++ src/test/java/com/rakovets/course/javabasics/practice/javaio/Demo2.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.javabasics.practice.javaio;
+import
+
+public class Demo2 {
+    public static void main(String[] args) {
+        getFileStrings();
+    }
+}
