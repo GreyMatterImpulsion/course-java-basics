4e73efadecf84ac3b8c1cec33321d221bf796e05
GreyMatterImpulsion <smaroleg@gmail.com>
2020-12-25 23:56:46 +0300

    Reflection tasks done

--- src/main/java/com/rakovets/course/javabasics/practice/reflection/ReflectionUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/reflection/ReflectionUtil.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.javabasics.practice.reflection;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ReflectionUtil {
     public static Field getField(Class<?> clazz, String fieldName) {
@@ -12,5 +14,38 @@ public class ReflectionUtil {
         return result;
     }
 
+    public static Method getMethod(Class<?> clazz, String methodName) {
+        Method result = null;
+        try {
+            result = clazz.getDeclaredMethod(methodName);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
+    public static Method[] getArrayOfMethods(Class<?> clazz) {
+        return clazz.getDeclaredMethods();
+    }
+
+    public static Object getInvokedMethod(Method method, Object object) {
+        Object result = null;
+        try {
+            result = method.invoke(object);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void setValueToField(Field field, Object object, Object value) {
+        field.setAccessible(true);
+        try {
+            field.set(object, value);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/rakovets/course/javabasics/practice/reflection/TestClass.java
+++ src/test/java/com/rakovets/course/javabasics/practice/reflection/TestClass.java
@@ -1,9 +1,41 @@
 package com.rakovets.course.javabasics.practice.reflection;
 
-public class TestClass {
-    public String str = "Azazaza";
 
-    public String mamkinMetod() {
+public class TestClass {
+    private String str;
+    private int number;
+
+    public TestClass(String str, int number) {
+        this.str = str;
+        this.number = number;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    private void printStr() {
+        System.out.println(str);
+    }
+
+    public String makeSomething() {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return str + number;
     }
 }
